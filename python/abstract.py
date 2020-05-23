9be3568d1dec4552c7f56b8e3414b868e938d6c0
hridkamolbiswas <hrid.bis@outlook.com>
2020-05-23 12:41:34 +0200

    sdsa

--- python/abstract.py
+++ python/abstract.py
@@ -1,8 +1,18 @@
-class Computer:
+from abc import ABC, abstractmethod
 
+class Computer(ABC):
+    @abstractmethod
     def process(self):
-        #print("runnung")
         pass
+
+class Laptop(Computer):
+    def process(self):
+        print('runnung')
+
+#com = Computer()
+com1 = Laptop()
+com1.process()
+
 
 
 
