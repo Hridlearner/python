a743e098ce5c5c1a666fab8da7c601e5f9685275
hridkamolbiswas <hrid.bis@outlook.com>
2020-05-24 16:22:36 +0200

    static method

--- python/class_method.py
+++ python/class_method.py
@@ -6,7 +6,7 @@ class Mobile:
     @classmethod
     def class_info(cls, ram, self):
         cls.ram= ram
-        print(cls.phone_type, cls.ram, self.kk)
+        print(cls.phone_type, cls.ram, cls.tution)
 
     def __init__(self):
         self.kk=20
@@ -14,10 +14,10 @@ class Mobile:
         print(self.phone_type)
 
     def info(self):
-        print(self.tution)#call class vriabe with instance
-
-    def update_tution(self):
-        self.tution = 3000    
+        print(self.tution)#call class vriabe with instanc
+    @classmethod
+    def update_tution(cls):
+        cls.tution = 3000    
 
 #Mobile.class_info('2gb')
 #print(Mobile.phone_type)
@@ -25,8 +25,10 @@ class Mobile:
 
 m=Mobile()
 print(m.kk)
-m.update_tution()
+Mobile.update_tution() #call class method by usisng class name (conventional)
 m.info()
+
+m2 = Mobile()
 #print(m.phone_type)
 #print(m.ram)
 
--- python/static_method.py
+++ python/static_method.py
@@ -1,5 +1,5 @@
 class Student:
-
+    uni = 'UPB'
     def __init__(self, name, ID):
         self.name = name
         self.ID =  ID
@@ -7,5 +7,14 @@ class Student:
 
     def get_age(self):
         pass
+    
+    #if we dont use any class or instance variable in a function
+    #its better to write as a static method
+    @staticmethod   
+    def get_info(x,y):
+        print(x,y)
+        #print(self.name)
+    
 
 s = Student('hrid',500)
+s.get_info(50,6,)
